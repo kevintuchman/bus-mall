3f7abdfded9ff1779753bafd9a8b0ef44289616c
kevintuchman <kevin.a.tuchman@gmail.com>
2019-09-21 21:17:06 -0700

    you can now click

--- js/app.js
+++ js/app.js
@@ -1,99 +1,87 @@
 'use strict';
+var recentRandomNumbers = [];
 
 var votesRemaining = 25;
 
 var busMallOneEl = document.getElementById('picture1');
 var busMallTwoEl = document.getElementById('picture2');
-var busMallThreeEl = document.getElementById('picture 3');
+var busMallThreeEl = document.getElementById('picture3');
 
 var containerEl = document.getElementById('busmall-container');
 
 var allBusMall = [];
 
-function busMall(name) {
+function BusMall(name) {
   this.alt = name;
   this.title = name;
-  this.src = `img/${name.jpg}`;
+  this.src = `img/${name}.jpg`;
   this.votes = 0;
   this.views = 0;
 
   allBusMall.push(this);
 }
 
-new BusMall('bag');
-new BusMall('banana');
-new BusMall('bathroom');
-new BusMall('boots');
-new BusMall('breakfast');
-new BusMall('bubblegum');
-new BusMall('chair');
-new BusMall('cthulhu');
-new BusMall('dog-duck');
-new BusMall('dragon');
-new BusMall('pen');
-new BusMall('pet-sweep');
-new BusMall('scissors');
-new BusMall('shark');
-new BusMall('sweep');
-new BusMall('tauntaun');
-new BusMall('unicorn');
-new BusMall('usb');
-new BusMall('water-can');
-new BusMall('wine-glass');
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function populateData(){
+  new BusMall('bag');
+  new BusMall('banana');
+  new BusMall('bathroom');
+  new BusMall('boots');
+  new BusMall('breakfast');
+  new BusMall('bubblegum');
+  new BusMall('chair');
+  new BusMall('cthulhu');
+  new BusMall('dog-duck');
+  new BusMall('dragon');
+  new BusMall('pen');
+  new BusMall('pet-sweep');
+  new BusMall('scissors');
+  new BusMall('shark');
+  new BusMall('sweep');
+  new BusMall('tauntaun');
+  new BusMall('unicorn');
+  new BusMall('usb');
+  new BusMall('water-can');
+  new BusMall('wine-glass');
+}
+
+
 
 function render() {
 
-  var randomxIndex = random(0, allBusMall.length - 1);
+  var randomIndex = random(0, allBusMall.length - 1);
 
-  while (recentRandomNumbers.includes(randomIndex)) {
-    randomIndex = random(0, allBusMall.length - 1);
-  }
-}
+  // while (recentRandomNumbers.includes(randomIndex)) {
+  //   randomIndex = random(0, allBusMall.length - 1);
+  // }
 
-if (recentRandomNumbers.length > 4) {
-  recentRandomNumbers.shift();
-}
-recentRandomNumbers.push(randomIndex)
+  // if (recentRandomNumbers.length > 3) {
+  //   recentRandomNumbers.shift();
+  // }
+  // recentRandomNumbers.push(randomIndex)
 
-allBusMall[randomIndex].views++;
+  allBusMall[randomIndex].views++;
+  console.log(randomIndex);
+  busMallOneEl.src = allBusMall[randomIndex].src;
+  busMallOneEl.alt = allBusMall[randomIndex].title;
+  busMallOneEl.title = allBusMall[randomIndex].title;
 
-busMallOneEl.src = allBusMall[randomIndex].filepath;
-busMallOneEl.alt = allBusMall[randomIndex].name;
-busMallOneEl.title = allBusMall[randomIndex].name;
+  var randomIndexTwo = random(0, allBusMall.length - 1);
+  allBusMall[randomIndexTwo].views++;
+  console.log(randomIndexTwo);
+  busMallTwoEl.src = allBusMall[randomIndexTwo].src;
+  busMallTwoEl.alt = allBusMall[randomIndexTwo].title;
+  busMallTwoEl.title = allBusMall[randomIndexTwo].title;
 
-var randomIndex = random(0, allBusMall.length - 1);
-
-while (recentRandomNumbers.includes(randomIndex)) {
-  randomIndex = random(0, allBusMall.length - 1);
-}
-
-if (recentRandomNumbers.length > 4) {
-  recentRandomNumbers.shift();
-}
-
-allBusMall[randomxIndex].views++;
-
-busMallTwoEl.src = allBusMall[randomIndex].filepath;
-busMallTwoEl.alt = allBusMall[randomIndex].name;
-busMallTwoEl.title = allBusMall[randomIndex].name;
-
-var randomxIndex = random(0, allBusMall.length - 1);
-
-while (recentRandomNumbers.includes(randomIndex)) {
-  randomIndex = random(0, allBusMall.length - 1);
-}
-
-if (recentRandomNumbers.length > 4) {
-  recentRandomNumbers.shift();
-}
-recentRandomNumbers.push(randomIndex)
-
-busMallThreeEl.src = allBusMall[randomIndex].filepath;
-busMallThreeEl.alt = allBusMall[randomIndex].name;
-busMallThreeEl.title = allBusMall[randomIndex].name;
-
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  var randomIndexThree = random(0, allBusMall.length - 1);
+  allBusMall[randomIndexThree].views++;
+  console.log(randomIndexThree);
+  busMallThreeEl.src = allBusMall[randomIndexThree].src;
+  busMallThreeEl.alt = allBusMall[randomIndexThree].title;
+  busMallThreeEl.title = allBusMall[randomIndexThree].title;
 }
 
 function imageGenerator() {
@@ -117,88 +105,89 @@ function imageGenerator() {
   busMallTwoEl.title = allBusMall[indexTwo].title;
 
   allBusMall[indexTwo].views++;
+
+  var indexThree = random(allBusMall.length);
+
+  busMallThreeEl.src = allBusMall[indexThree].src;
+  busMallThreeEl.alt = allBusMall[indexThree].alt;
+  busMallThreeEl.title = allBusMall[indexThree].title;
+
+  allBusMall[indexThree].views++;
 }
-
-busMallThreeEl.src = allBusMall[indexThree].src;
-busMallThreeEl.alt = allBusMall[indexThree].alt;
-busMallThreeEl.title = allBusMall[indexThree].title;
-
-allBusMall[indexThree].views++;
 
 //clicker
 
 function handleClick(event) {
   var clickedBusMall = event.target.title;
-
-  if (e.target.id === 'busmall-container') {
-    alert('click');
-  }
-
-  if (votesRemaining === 0) {
-    busMallContainerEl.removeEventListener('click', handleClick);
-    // render the results to the DOM
-    renderChart();
-  }
-
+  console.log('You clicked on', clickedBusMall);
   for (var i = 0; i < allBusMall.length; i++) {
     if (clickedBusMall === allBusMall[i].title) {
       allBusMall[i].votes++;
+      votesRemaining = votesRemaining - 1;
     }
   }
-  render();
-  imageGenerator();
+
+  if (votesRemaining === 0) {
+    console.log('voting is done');
+    containerEl.removeEventListener('click', handleClick);
+    // render the results to the DOM
+  //   renderChart();
+  }
+  else{
+    render();
+  }
 }
 
-busMallContainerEl.addEventListener('click, handleClick');
 containerEl.addEventListener('click', handleClick);
-
-imageGenerator();
+//main functions
+populateData();
+console.log(allBusMall);
 render();
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    }
-  }
-});
+// var ctx = document.getElementById('myChart').getContext('2d');
+// var myChart = new Chart(ctx, {
+//   type: 'bar',
+//   data: {
+//     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//     datasets: [{
+//       label: '# of Votes',
+//       data: [12, 19, 3, 5, 2, 3],
+//       backgroundColor: [
+//         'rgba(255, 99, 132, 0.2)',
+//         'rgba(54, 162, 235, 0.2)',
+//         'rgba(255, 206, 86, 0.2)',
+//         'rgba(75, 192, 192, 0.2)',
+//         'rgba(153, 102, 255, 0.2)',
+//         'rgba(255, 159, 64, 0.2)'
+//       ],
+//       borderColor: [
+//         'rgba(255, 99, 132, 1)',
+//         'rgba(54, 162, 235, 1)',
+//         'rgba(255, 206, 86, 1)',
+//         'rgba(75, 192, 192, 1)',
+//         'rgba(153, 102, 255, 1)',
+//         'rgba(255, 159, 64, 1)'
+//       ],
+//       borderWidth: 1
+//     }]
+//   },
+//   options: {
+//     scales: {
+//       yAxes: [{
+//         ticks: {
+//           beginAtZero: true
+//         }
+//       }]
+//     }
+//   }
+// });
 
 //local storage
 
-var stringifyBusMall = JSON.stringify(allBusMall);
+// var stringifyBusMall = JSON.stringify(allBusMall);
 
-localStorage.setItem('busmall', 'stringifyedBusMall');
+// localStorage.setItem('busmall', 'stringifyedBusMall');
 
-var localStorageBusMall = localStorage.getItem('busmall');
+// var localStorageBusMall = localStorage.getItem('busmall');
 
-var parsedBusMall = JSON.parse(localStorageBusMall);
+// var parsedBusMall = JSON.parse(localStorageBusMall);
